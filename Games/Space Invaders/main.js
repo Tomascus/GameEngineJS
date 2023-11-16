51c5165787fff2f169974468cd093a4630ebde77
Tomas Szabo <bady.tomas33@gmail.com>
2023-11-16 12:31:43 +0000

    added alien class

--- Games/Space Invaders/main.js
+++ Games/Space Invaders/main.js
@@ -28,6 +28,11 @@ class Player {
             this.x = canvas.width - this.width;
         }
     }
+
+    fire() {
+        const bullet = new Bullet(this.x + this.width/2-2.5, this.y, 5, 10, 7);
+        bullets.push(bullet);
+    }
 }
 
 class Bullet {
@@ -50,6 +55,22 @@ class Bullet {
     }
 }
 
+class Alien {
+
+    constructor(x,y,width,height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    draw() {
+        ctx.fillStyle = "green";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+}
+
 const alienRows = 4;
 const alienColumns = 10;
 const alienWidth = 40;
@@ -57,6 +78,9 @@ const alienHeight = 30;
 const alienPadding = 10;
 let alienDirection = 1;
 let alienMoveDown = false;
+
+const player = new Player(canvas.width/2 -25, canvas.height -50,50,20);
+let bullets = [];
 
 const keyStates = {
 
